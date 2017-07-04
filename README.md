935f030477bb7f6677f17a47425ea270a5e1d0b1
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-07-04 19:04:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+YOLO Jukebox
+============
+
+Trigger kload's voice from your command line <3
+
+Usage
+-----
+
+```
+source aliases.sh
+```
+
+then :
+
+```
+$ sleep 1 && itworks || doesntwork
+$ unknowncommand && itworks || doesntwork
+$ veryexpert
+```
---
+++ aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/sounds"
+
+alias bulb="paplay $DIR/bulb.ogg"
+alias comeoninternet="paplay $DIR/comeoninternet.ogg"
+alias damned="paplay $DIR/damned.ogg"
+alias doesntwork="paplay $DIR/doesntwork.ogg"
+alias freifunk="paplay $DIR/freifunk.ogg"
+alias hmm="paplay $DIR/hmm.ogg"
+alias hoo="paplay $DIR/hoo.ogg"
+alias itsgone="paplay $DIR/itsgone.ogg"
+alias itworks="paplay $DIR/itworks.ogg"
+alias noice="paplay $DIR/noice.ogg"
+alias okay="paplay $DIR/okay.ogg"
+alias ookay="paplay $DIR/ookay.ogg"
+alias thankyou="paplay $DIR/thankyou.ogg"
+alias veryexpert="paplay $DIR/veryexpert.ogg"
